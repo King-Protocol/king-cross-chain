cd1d71024440e9cc353109774ab19f4ac52518ff
artemwoofsoftware <artem.m@woof.software>
2025-02-12 15:37:30 -0600

    feat: deploy script

--- deploy/KingOFTL1.ts
+++ deploy/KingOFTL1.ts
@@ -1,48 +1,83 @@
-import assert from 'assert'
+import { DeployFunction } from 'hardhat-deploy/types';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import assert from 'assert';
+import dotenv from 'dotenv';
 
-import { type DeployFunction } from 'hardhat-deploy/types'
+dotenv.config();
 
-const contractName = 'KingOFTL1'
+const owner = process.env.L1_OWNER_ADDRESS;
 
-const deploy: DeployFunction = async (hre) => {
-    const { getNamedAccounts, deployments } = hre
-    
-    const { deploy } = deployments
-    const { deployer } = await getNamedAccounts()
+const deploy: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+    const { deployments, getNamedAccounts, ethers, network } = hre;
+    const { deploy, log } = deployments;
+    const { deployer } = await getNamedAccounts();
 
-    assert(deployer, 'Missing named deployer account')
+    assert(deployer, 'Missing named deployer account');
+    assert(owner, 'Missing owner account');
 
     const expectedNetworkId = 1;
-    if (hre.network.config.chainId !== expectedNetworkId) {
-        console.error(`Invalid network ID: ${hre.network.config.chainId}. Expected: ${expectedNetworkId}`)
-        return
+    const peerNetworkId = 1923;
+    if (network.config.chainId !== expectedNetworkId) {
+        console.error(
+            `Invalid network ID: ${network.config.chainId}. Expected: ${expectedNetworkId}`
+        );
+        return;
     }
 
-    console.log(`Network: ${hre.network.name}`)
-    console.log(`Deployer: ${deployer}`)
+    log(`Network: ${network.name}`);
+    log(`Deployer: ${deployer}`);
 
-    const endpointV2Deployment = await hre.deployments.get('EndpointV2')
-
-    if (hre.network.config.oftAdapter == null) {
-        console.warn(`oftAdapter not configured on network config, skipping OFTWrapper deployment`)
-
-        return
+    if (!network.config.oftAdapter || !network.config.oftAdapter.tokenAddress) {
+        console.warn(
+            `oftAdapter not configured on network config, skipping KingOFTL1 deployment`
+        );
+        return;
     }
 
-    const { address } = await deploy(contractName, {
+    const endpointV2Deployment = await deployments.get('EndpointV2');
+
+    const L1ImplDeployment = await deploy('KingOFTL1', {
         from: deployer,
         args: [
-            hre.network.config.oftAdapter.tokenAddress,
-            endpointV2Deployment.address,
-            deployer,
+            network.config.oftAdapter.tokenAddress,
+            endpointV2Deployment.address
         ],
         log: true,
         skipIfAlreadyDeployed: false,
-    })
+    });
 
-    console.log(`Deployed contract: ${contractName}, network: ${hre.network.name}, address: ${address}`)
-}
+    const L1ImplAddress = L1ImplDeployment.address;
+    log(`KingOFTL1implementation deployed at ${L1ImplAddress}`);
 
-deploy.tags = [contractName]
+    const UUPSDeployment = await deploy('UUPS', {
+        from: deployer,
+        args: [
+            L1ImplAddress,
+            deployer,
+            "0x",
+        ],
+        log: true,
+        skipIfAlreadyDeployed: false,
+    });
 
-export default deploy
+    const l1ProxyAddress = UUPSDeployment.address;
+    log(`UUPS proxy deployed at ${l1ProxyAddress}`);
+
+    const kingOFTL1 = await ethers.getContractAt('KingOFTL1Mock', l1ProxyAddress);
+
+    const initTx = await kingOFTL1.initialize(deployer, deployer);
+    await initTx.wait();
+    const rateLimit = [[peerNetworkId, ethers.utils.parseEther('100000'), 1]];
+    await kingOFTL1.connect(deployer).setInboundRateLimits(rateLimit);
+    await kingOFTL1.connect(deployer).setOutboundRateLimits(rateLimit);
+    await kingOFTL1.connect(deployer).transferOwnership(owner);
+    await kingOFTL1.connect(deployer).setDelegate(owner);
+    
+
+    log(
+        `Deployment complete: KingOFTL1 proxy is deployed at ${l1ProxyAddress} on network ${network.name}`
+    );
+};
+
+deploy.tags = ['KingOFTL1'];
+export default deploy;
--- deploy/KingOFTL2.ts
+++ deploy/KingOFTL2.ts
@@ -1,48 +1,78 @@
 import assert from 'assert'
+import { DeployFunction } from 'hardhat-deploy/types'
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+import dotenv from 'dotenv'
 
-import { type DeployFunction } from 'hardhat-deploy/types'
+dotenv.config()
 
-const contractName = 'KingOFTL2'
-
-const deploy: DeployFunction = async (hre) => {
-    const { getNamedAccounts, deployments } = hre
-
-    const { deploy } = deployments
+const owner = process.env.L2_OWNER_ADDRESS
+const deploy: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+    const { getNamedAccounts, deployments, ethers, network } = hre
+    const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
 
     assert(deployer, 'Missing named deployer account')
 
     const expectedNetworkId = 1923;
-    if (hre.network.config.chainId !== expectedNetworkId) {
-        console.error(`Invalid network ID: ${hre.network.config.chainId}. Expected: ${expectedNetworkId}`)
+    const peerNetworkId = 1;
+
+    if (network.config.chainId !== expectedNetworkId) {
+        console.error(
+            `Invalid network ID: ${network.config.chainId}. Expected: ${expectedNetworkId}`
+        )
         return
     }
 
-    console.log(`Network: ${hre.network.name}`)
+    console.log(`Network: ${network.name}`)
     console.log(`Deployer: ${deployer}`)
 
-    const endpointV2Deployment = await hre.deployments.get('EndpointV2')
+    const endpointV2Deployment = await deployments.get('EndpointV2')
 
     if (hre.network.config.oftAdapter != null) {
-        console.warn(`oftAdapter configuration found on OFT deployment, skipping OFT deployment`)
+        console.warn(
+            `oftAdapter configuration found on network config, skipping L2 deployment`
+        )
         return
     }
 
-    const { address } = await deploy(contractName, {
+    const l2ImplDeployment = await deploy('KingOFTL2', {
         from: deployer,
         args: [
-            'King Token',
-            'KING',
             endpointV2Deployment.address,
-            deployer,
         ],
         log: true,
         skipIfAlreadyDeployed: false,
     })
+    const l2ImplAddress = l2ImplDeployment.address
+    console.log(`KingOFTL2 implementation deployed at: ${l2ImplAddress}`)
 
-    console.log(`Deployed contract: ${contractName}, network: ${hre.network.name}, address: ${address}`)
+    const proxyDeployment = await deploy('UUPS', {
+        from: deployer,
+        args: [
+            l2ImplAddress,
+            deployer,
+            "0x",
+        ],
+        log: true,
+        skipIfAlreadyDeployed: false,
+    })
+    const l2ProxyAddress = proxyDeployment.address
+    console.log(`UUPS proxy deployed at: ${l2ProxyAddress}`)
+
+    const kingOFTL2 = await ethers.getContractAt('KingOFTL2', l2ProxyAddress)
+    const rateLimit = [[peerNetworkId, ethers.utils.parseEther('100000'), 1]];
+    await kingOFTL2.connect(deployer).setInboundRateLimits(rateLimit);
+    await kingOFTL2.connect(deployer).setOutboundRateLimits(rateLimit);
+
+    const initTx = await kingOFTL2.initialize("KingOFTL2", "KING", deployer)
+    await initTx.wait()
+    await kingOFTL2.connect(deployer).transferOwnership(owner);
+    await kingOFTL2.connect(deployer).setDelegate(owner);
+    
+    log(
+        `Deployment complete: KingOFTL2 proxy is deployed at ${l2ProxyAddress} on network ${network.name}`
+    );
 }
 
-deploy.tags = [contractName]
-
+deploy.tags = ['KingOFTL2']
 export default deploy
