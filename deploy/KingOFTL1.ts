6fc7672e5059a59cfa7b53aa0721a8c8ca645489
artemwoofsoftware <artem.m@woof.software>
2025-02-13 20:28:19 -0600

    feat: deployment

--- deploy/KingOFTL1.ts
+++ deploy/KingOFTL1.ts
@@ -2,6 +2,7 @@ import { DeployFunction } from 'hardhat-deploy/types';
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import assert from 'assert';
 import dotenv from 'dotenv';
+import { EndpointId } from '@layerzerolabs/lz-definitions';
 
 dotenv.config();
 
@@ -16,16 +17,16 @@ const deploy: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
     assert(owner, 'Missing owner account');
 
     const expectedNetworkId = 1;
-    const peerNetworkId = 1923;
+    const peerNetworkEId = EndpointId.SWELL_V2_MAINNET;
+    log(`Network: ${network.name}`);
+    log(`Deployer: ${deployer}`);
+
     if (network.config.chainId !== expectedNetworkId) {
         console.error(
             `Invalid network ID: ${network.config.chainId}. Expected: ${expectedNetworkId}`
         );
         return;
     }
-
-    log(`Network: ${network.name}`);
-    log(`Deployer: ${deployer}`);
 
     if (!network.config.oftAdapter || !network.config.oftAdapter.tokenAddress) {
         console.warn(
@@ -67,18 +68,23 @@ const deploy: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
 
     const initTx = await kingOFTL1.initialize(deployer, deployer);
     await initTx.wait();
-    const rateLimit = [[peerNetworkId, ethers.utils.parseEther('100000'), 1]];
-    await kingOFTL1.connect(deployer).setInboundRateLimits(rateLimit);
-    await kingOFTL1.connect(deployer).setOutboundRateLimits(rateLimit);
-   
-     
-    await kingOFTL1.connect(deployer).transferOwnership(owner);
-    await kingOFTL1.connect(deployer).setDelegate(owner);
+
+    const signer = await ethers.getSigner(deployer);
+    if (!signer) {
+        throw new Error('Deployer signer not found');
+    }
+    const rateLimit = [[peerNetworkEId, ethers.utils.parseEther('100000'), 1]];
+    await kingOFTL1.connect(signer).setInboundRateLimits(rateLimit);
+    await kingOFTL1.connect(signer).setOutboundRateLimits(rateLimit);
+    
+
+    await kingOFTL1.connect(signer).setDelegate(owner);
+    await kingOFTL1.connect(signer).transferOwnership(owner);
 
     log(
         `Deployment complete: KingOFTL1 proxy is deployed at ${l1ProxyAddress} on network ${network.name}`
     );
 };
 
-deploy.tags = ['KingOFTL1'];
+deploy.tags = ['KingOFTL2'];
 export default deploy;
--- layerzero.config.ts
+++ layerzero.config.ts
@@ -3,12 +3,12 @@ import { EndpointId } from '@layerzerolabs/lz-definitions'
 import type { OAppOmniGraphHardhat, OmniPointHardhat } from '@layerzerolabs/toolbox-hardhat'
 
 const mainnetContract: OmniPointHardhat = {
-    eid: EndpointId.ETHEREUM_MAINNET,
+    eid: EndpointId.ETHEREUM_V2_MAINNET,
     contractName: 'KingOFTL1',
 }
 
 const swellContract: OmniPointHardhat = {
-    eid: EndpointId.SWELL_MAINNET,
+    eid: EndpointId.SWELL_V2_MAINNET,
     contractName: 'KingOFTL2',
 }
 
@@ -25,10 +25,85 @@ const config: OAppOmniGraphHardhat = {
         {
             from: mainnetContract,
             to: swellContract,
+            config: {
+                sendLibrary: "0xbB2Ea70C9E858123480642Cf96acbcCE1372dCe1",
+                receiveLibraryConfig: {
+                    receiveLibrary: "0xc02Ab410f0734EFa3F14628780e6e695156024C2",
+                    gracePeriod: BigInt(0),
+                },
+                receiveLibraryTimeoutConfig: {
+                    lib: "0xc02Ab410f0734EFa3F14628780e6e695156024C2",
+                    expiry: BigInt(0),
+                },
+                sendConfig: {
+                    executorConfig: {
+                        maxMessageSize: 10000,
+                        executor: "0x173272739Bd7Aa6e4e214714048a9fE699453059",
+                    },
+                    ulnConfig: {
+                        confirmations: BigInt(0),
+                        requiredDVNs: ["0x589dedbd617e0cbcb916a9223f4d1300c294236b"],
+                        optionalDVNs: [
+                            "0x380275805876ff19055ea900cdb2b46a94ecf20d",
+                            "0xa59ba433ac34d2927232918ef5b2eaafcf130ba5",
+                        ],
+                        optionalDVNThreshold: 1,
+                    },
+                },
+                receiveConfig: {
+                    ulnConfig: {
+                        confirmations: BigInt(0),
+                        requiredDVNs:  ["0x589dedbd617e0cbcb916a9223f4d1300c294236b"],
+                        optionalDVNs: [
+                            "0x380275805876ff19055ea900cdb2b46a94ecf20d",
+                            "0xa59ba433ac34d2927232918ef5b2eaafcf130ba5",
+                        ],
+                        optionalDVNThreshold: 1,
+                    },
+                },
+            }
         },
         {
             from: swellContract,
             to: mainnetContract,
+            config: {
+                sendLibrary: "0xc1B621b18187F74c8F6D52a6F709Dd2780C09821",
+                receiveLibraryConfig: {
+                    receiveLibrary: "0x377530cdA84DFb2673bF4d145DCF0C4D7fdcB5b6",
+                    gracePeriod: BigInt(0),
+                },
+                receiveLibraryTimeoutConfig: {
+                    lib: "0x377530cdA84DFb2673bF4d145DCF0C4D7fdcB5b6",
+                    expiry: BigInt(0),
+                },
+                sendConfig: {
+                    executorConfig: {
+                        maxMessageSize: 10000,
+                        executor: "0xa20DB4Ffe74A31D17fc24BD32a7DD7555441058e",
+                    },
+                    ulnConfig: {
+                        confirmations: BigInt(0),
+                        requiredDVNs: ["0x6788f52439aca6bff597d3eec2dc9a44b8fee842"],
+                        optionalDVNs: [
+                            "0xdd7b5e1db4aafd5c8ec3b764efb8ed265aa5445b",
+                            "0xf4672690ef45b46eaa3b688fe2f0fc09e9366b20"
+                        ],
+                        optionalDVNThreshold: 1,
+                    },
+                },
+                receiveConfig: {
+                    ulnConfig: {
+                        confirmations: BigInt(0),
+                        requiredDVNs: ["0x6788f52439aca6bff597d3eec2dc9a44b8fee842"],
+                        optionalDVNs: [
+                            "0xdd7b5e1db4aafd5c8ec3b764efb8ed265aa5445b",
+                            "0xf4672690ef45b46eaa3b688fe2f0fc09e9366b20"
+                        ],
+                        optionalDVNThreshold: 1,
+                    },
+                },
+                
+            }
         },
     ],
 }
